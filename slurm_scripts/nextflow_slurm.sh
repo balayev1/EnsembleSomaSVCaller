aa8e54e6240f60b529b4da55a14e9a7eab43ff0e
balayev1 <64073191+balayev1@users.noreply.github.com>
2026-01-28 00:25:01 +0000

    Added slurm file

---
+++ slurm_scripts/nextflow_slurm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=ASCAT_Master
+#SBATCH --partition=asvnode1,msibigmem
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16G
+#SBATCH --time=24:00:00
+#SBATCH --output=ascat_master_%j.out
+#SBATCH --error=ascat_master_%j.err
+
+# Load modules
+module load nextflow
+module load singularity
+
+# Run Nextflow
+nextflow run main.nf \
+    -profile singularity \
+    -resume \
+    --config nextflow.config \
+    -with-report report.html \
+    -with-timeline timeline.html
+
+echo "ASCAT Pipeline Finished at $(date)"
