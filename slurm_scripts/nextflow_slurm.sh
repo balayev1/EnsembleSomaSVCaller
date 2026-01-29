e3573072ebb7868395f316b78a6fef62d6d95451
balayev1 <64073191+balayev1@users.noreply.github.com>
2026-01-29 16:40:26 +0000

    slurm

--- slurm_scripts/nextflow_slurm.sh
+++ slurm_scripts/nextflow_slurm.sh
@@ -2,10 +2,12 @@
 #SBATCH --job-name=ASCAT_Master
 #SBATCH --partition=asvnode1,msibigmem
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=16G
+#SBATCH --mem=40G
 #SBATCH --time=24:00:00
 #SBATCH --output=ascat_master_%j.out
 #SBATCH --error=ascat_master_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=balay011@umn.edu
 
 # Load modules
 module load nextflow
