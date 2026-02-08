a4cde08626af5fe50abddcb9d7994923b9318e82
balayev1 <64073191+balayev1@users.noreply.github.com>
2026-02-08 20:30:19 +0000

    Master script

--- master_somasv.sh
+++ master_somasv.sh
@@ -1,48 +1,59 @@
 #!/bin/bash
 
+# master_somasv.sh
+# Master script to run nf-aceseq and EnsembleSomaSVCaller pipelines on SLURM
+# author: balayev1 & gemini pro & claude sonnet 4.5
+# Usage: bash master_somasv.sh
+
 # Load modules
 module load nextflow
 module load singularity
 
-# Singularity cachedir
-export NXF_SINGULARITY_CACHEDIR="/scratch.global/balay011/singularity_cache"
+# TODO: SET SINGULARITY CACHE DIR
+export NXF_SINGULARITY_CACHEDIR="path/to/singularity_cache"
 mkdir -p "$NXF_SINGULARITY_CACHEDIR"
 
-# Define github repo paths (Define these early so we can use them)
-BASE_DIR="$PWD"
-ACESEQ_REPO="${BASE_DIR}/nf-aceseq"
-SOMASV_REPO="${BASE_DIR}/EnsembleSomaSVCaller"
+# TODO: DEFINE PATHS TO GITHUB REPOS
+ACESEQ_REPO="path/to/nf-aceseq"
+SOMASV_REPO="path/to/EnsembleSomaSVCaller"
 
-# ----------------------------------------------------
-# 1. Clone & Prep Repos
-# ----------------------------------------------------
+# # ----------------------------------------------------
+# # 1. Clone & Prep Repos
+# # ----------------------------------------------------
 
-# Clone nf-aceseq
-if [ ! -d "$ACESEQ_REPO" ]; then
-    echo "Cloning nf-aceseq..."
-    git clone https://github.com/ghga-de/nf-aceseq.git
-fi
-# ALWAYS ensure bin is executable (even if repo existed previously)
-chmod +x "${ACESEQ_REPO}/bin/"*
+# # Clone nf-aceseq
+# if [ ! -d "$ACESEQ_REPO" ]; then
+#     echo "Cloning nf-aceseq..."
+#     git clone https://github.com/ghga-de/nf-aceseq.git
+# fi
+# # ALWAYS ensure bin is executable (even if repo existed previously)
+# chmod +x nf-aceseq/bin/
 
-# Clone EnsembleSomaSVCaller
-if [ ! -d "$SOMASV_REPO" ]; then
-    echo "Cloning EnsembleSomaSVCaller..."
-    git clone https://github.com/balayev1/EnsembleSomaSVCaller.git
-fi
+# # Clone EnsembleSomaSVCaller
+# if [ ! -d "$SOMASV_REPO" ]; then
+#     echo "Cloning EnsembleSomaSVCaller..."
+#     git clone https://github.com/balayev1/EnsembleSomaSVCaller.git
+# fi
 
 # ----------------------------------------------------
 # 2. Set Arguments
 # ----------------------------------------------------
 
-# TODO: UPDATE & ADD AS NEEDED!
-export ACESEQ_INPUT="${BASE_DIR}/samplesheet.csv" 
-export ACESEQ_OUTPUT="${BASE_DIR}/ACESEQ_out"
-
+# TODO: UPDATE & ADD ARGUMENTS AS NEEDED! CHECK REPO FOR MORE INFO!
+## GENERAL ARGUMENTS
 export PROFILE="singularity"
+
+## ACESEQ-SPECIFIC ARGUMENTS 
+export ACESEQ_INPUT="path/to/samplesheet.csv" 
+export ACESEQ_OUTPUT="path/to/ACESEQ_out"
+export BEAGLE_REF="path/to/beagle_ref"
+export BEAGLE_REF_EXT="vcf"
+export BEAGLE_GMAP="path/to/beagle_gmap/no_chr_in_chrom_field"
+export BEAGLE_GMAP_EXT="map"
+export GENOME_VER="GRCh38"
 export MPILEUP_QUAL=20
 export SNP_MIN_COVERAGE=15
-export GENOME_VER="GRCh38"
+export REFGENIE_IGNORE=true
 
 # ----------------------------------------------------
 # 3. Submit Jobs
@@ -54,10 +65,10 @@ mkdir -p "${SOMASV_REPO}/logs"
 
 # --- JOB 1: nf-aceseq ---
 cd "$ACESEQ_REPO" || exit
-ACESEQ_JOB=$(sbatch --parsable "${SOMASV_REPO}/nf_aceseq.sbatch")
+ACESEQ_JOB=$(sbatch --parsable "${SOMASV_REPO}/slurm/nf_aceseq.sbatch")
 echo "Submitted ACEseq job: $ACESEQ_JOB"
 
 # --- JOB 2: SomaSV (Dependent) ---
 cd "$SOMASV_REPO" || exit
-SOMASV_JOB=$(sbatch --parsable --dependency=afterok:${ACESEQ_JOB} "${SOMASV_REPO}/nf_somasv.sbatch")
+SOMASV_JOB=$(sbatch --parsable --dependency=afterok:${ACESEQ_JOB} "${SOMASV_REPO}/slurm/nf_somasv.sbatch")
 echo "Submitted SomaSV job: $SOMASV_JOB (Waiting for $ACESEQ_JOB)"
